bfdca9306eae2d7d9a3524be2e61bdbc8a6738c7
Luca Demetrio <luca.demetrio@outlook.it>
2023-03-06 16:38:25 +0100

    Reverting example script to previous one

--- src/examples/run_evasion_attack.py
+++ src/examples/run_evasion_attack.py
@@ -1,4 +1,3 @@
-import torch
 import torchvision.datasets
 from torch.utils.data import DataLoader, Subset
 from src.adv.backends import Backends
@@ -11,13 +10,12 @@ from src.models.pytorch.base_pytorch_nn import BasePytorchClassifier
 from robustbench.utils import load_model
 
 net = load_model(model_name="Rony2019Decoupling", dataset="cifar10", threat_model="L2")
-device = "mps"
-net.to(device)
+net.to("mps")
 test_dataset = torchvision.datasets.CIFAR10(
     transform=torchvision.transforms.ToTensor(), train=False, root=".", download=True
 )
 test_dataset = Subset(test_dataset, list(range(5)))
-test_data_loader = DataLoader(test_dataset, batch_size=5, shuffle=False)
+test_data_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
 
 # Wrap model
 model = BasePytorchClassifier(net)
@@ -27,44 +25,17 @@ accuracy = Accuracy()(model, test_data_loader)
 print(accuracy)
 
 # Create and run attack
-epsilon = 0.5
-num_steps = 50
-step_size = 0.05
-perturbation_model = PerturbationModels.LINF
-y_target = None
-native_attack = PGD(
-    perturbation_model=perturbation_model,
-    epsilon=epsilon,
-    num_steps=num_steps,
-    step_size=step_size,
-    random_start=False,
-    y_target=y_target,
-    backend=Backends.NATIVE,
-)
-native_adv_ds = native_attack(model, test_data_loader)
-
-# Test accuracy on adversarial examples
-n_robust_accuracy = Accuracy()(model, native_adv_ds)
-print(n_robust_accuracy)
-
-# Create and run attack
-foolbox_attack = PGD(
-    perturbation_model=perturbation_model,
-    epsilon=epsilon,
-    num_steps=num_steps,
-    step_size=step_size,
+attack = PGD(
+    perturbation_model=PerturbationModels.LINF,
+    epsilon=0.5,
+    num_steps=50,
+    step_size=0.05,
     random_start=False,
     y_target=None,
-    backend=Backends.FOOLBOX,
+    backend=Backends.NATIVE,
 )
-f_adv_ds = foolbox_attack(model, test_data_loader)
+adv_ds = attack(model, test_data_loader)
 
 # Test accuracy on adversarial examples
-f_robust_accuracy = Accuracy()(model, f_adv_ds)
-print(f_robust_accuracy)
-
-native_data, native_labels = next(iter(native_adv_ds))
-f_data, f_labels = next(iter(f_adv_ds))
-
-distance = torch.linalg.norm(native_data.flatten(start_dim=1).to(device) - f_data.flatten(start_dim=1), ord=2, dim=1)
-print(distance)
+robust_accuracy = Accuracy()(model, adv_ds)
+print(robust_accuracy)
