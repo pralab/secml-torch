afb277a0aa5e94751466abb2c417ea0b17eb5807
maurapintor <maura.pintor@gmail.com>
2023-09-21 17:36:50 +0200

    extracted outer loop for batch iterations

--- secml2/adv/evasion/composite_attack.py
+++ secml2/adv/evasion/composite_attack.py
@@ -74,44 +74,34 @@ class CompositeEvasionAttack(BaseEvasionAttack):
     def init_perturbation_constraints(self) -> List[Constraint]:
         raise NotImplementedError("Must be implemented accordingly")
 
-    def __call__(self, model: BaseModel, data_loader: DataLoader) -> DataLoader:
-        adversarials = []
-        original_labels = []
+    def _run(
+        self, model: BaseModel, samples: torch.Tensor, labels: torch.Tensor
+    ) -> torch.Tensor:
         multiplier = 1 if self.y_target is not None else -1
         perturbation_constraints = self.init_perturbation_constraints()
-        for samples, labels in data_loader:
-            target = (
-                torch.zeros_like(labels) + self.y_target
-                if self.y_target is not None
-                else labels
-            ).type(labels.dtype)
-            delta = self.initializer(samples.data)
-            delta.requires_grad = True
-            optimizer = self.optimizer_cls([delta], lr=self.step_size)
-            x_adv = self.manipulation_function(samples, delta)
-            for i in range(self.num_steps):
-                scores = model.decision_function(x_adv)
-                target = target.to(scores.device)
-                loss = self.loss_function(scores, target)
-                loss = loss * multiplier
-                optimizer.zero_grad()
-                loss.backward()
-                delta.grad.data = self.gradient_processing(delta.grad.data)
-                optimizer.step()
-                for constraint in perturbation_constraints:
-                    delta.data = constraint(delta.data)
-                x_adv.data = self.manipulation_function(samples.data, delta.data)
-                for constraint in self.domain_constraints:
-                    x_adv.data = constraint(x_adv.data)
-                delta.data = self.manipulation_function.invert(samples.data, x_adv.data)
 
-            adversarials.append(x_adv)
-            original_labels.append(labels)
-
-        adversarials = torch.vstack(adversarials)
-        original_labels = torch.hstack(original_labels)
-        adversarial_dataset = TensorDataset(adversarials, original_labels)
-        adversarial_loader = DataLoader(
-            adversarial_dataset, batch_size=data_loader.batch_size
-        )
-        return adversarial_loader
+        target = (
+            torch.zeros_like(labels) + self.y_target
+            if self.y_target is not None
+            else labels
+        ).type(labels.dtype)
+        delta = self.initializer(samples.data)
+        delta.requires_grad = True
+        optimizer = self.optimizer_cls([delta], lr=self.step_size)
+        x_adv = self.manipulation_function(samples, delta)
+        for i in range(self.num_steps):
+            scores = model.decision_function(x_adv)
+            target = target.to(scores.device)
+            loss = self.loss_function(scores, target)
+            loss = loss * multiplier
+            optimizer.zero_grad()
+            loss.backward()
+            delta.grad.data = self.gradient_processing(delta.grad.data)
+            optimizer.step()
+            for constraint in perturbation_constraints:
+                delta.data = constraint(delta.data)
+            x_adv.data = self.manipulation_function(samples.data, delta.data)
+            for constraint in self.domain_constraints:
+                x_adv.data = constraint(x_adv.data)
+            delta.data = self.manipulation_function.invert(samples.data, x_adv.data)
+        return x_adv
