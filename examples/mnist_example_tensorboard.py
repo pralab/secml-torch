8380ac947445db024aa15f1aa53d1edcf92cc269
maurapintor <maura.pintor@gmail.com>
2024-02-22 17:06:54 +0100

    mnist example with tensorboard

--- examples/mnist_example_tensorboard.py
+++ examples/mnist_example_tensorboard.py
@@ -1,8 +1,11 @@
 import os
+from secml2.trackers.image_trackers import GradientsTracker, SampleTracker
 from secml2.trackers.trackers import (
+    GradientNormTracker,
     LossTracker,
     PredictionTracker,
     PerturbationNormTracker,
+    TensorboardTracker,
 )
 import torch
 import torchvision.datasets
@@ -56,9 +59,9 @@ accuracy = Accuracy()(model, test_data_loader)
 print(accuracy)
 
 # Create and run attack
-epsilon = 0.3
-num_steps = 10
-step_size = 0.05
+epsilon = 0.2
+num_steps = 200
+step_size = 0.01
 perturbation_model = PerturbationModels.LINF
 y_target = None
 
@@ -66,7 +69,12 @@ trackers = [
     LossTracker(),
     PredictionTracker(),
     PerturbationNormTracker("linf"),
+    GradientNormTracker(),
+    SampleTracker(),
+    GradientsTracker(),
 ]
+
+tensorboard_tracker = TensorboardTracker("logs/pgd", trackers)
 
 native_attack = PGD(
     perturbation_model=perturbation_model,
@@ -76,41 +84,6 @@ native_attack = PGD(
     random_start=False,
     y_target=y_target,
     backend=Backends.NATIVE,
-    trackers=trackers,
+    trackers=tensorboard_tracker,
 )
 native_adv_ds = native_attack(model, test_data_loader)
-
-for tracker in trackers:
-    print(tracker.name)
-    print(tracker.get())
-
-# Test accuracy on adversarial examples
-n_robust_accuracy = Accuracy()(model, native_adv_ds)
-print("robust accuracy foolbox: ", n_robust_accuracy)
-
-# Create and run attack
-foolbox_attack = PGD(
-    perturbation_model=perturbation_model,
-    epsilon=epsilon,
-    num_steps=num_steps,
-    step_size=step_size,
-    random_start=False,
-    y_target=y_target,
-    backend=Backends.FOOLBOX,
-)
-f_adv_ds = foolbox_attack(model, test_data_loader)
-
-# Test accuracy on adversarial examples
-f_robust_accuracy = Accuracy()(model, f_adv_ds)
-print("robust accuracy native: ", f_robust_accuracy)
-
-native_data, native_labels = next(iter(native_adv_ds))
-f_data, f_labels = next(iter(f_adv_ds))
-real_data, real_labels = next(iter(test_data_loader))
-
-distance = torch.linalg.norm(
-    native_data.flatten(start_dim=1).to(device) - f_data.flatten(start_dim=1),
-    ord=float("inf"),
-    dim=1,
-)
-print("Solutions are :", distance, "linf distant")
