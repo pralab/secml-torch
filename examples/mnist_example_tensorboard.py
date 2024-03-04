cb83152be54629ed76cde7873b8530ea8f3b6f8f
maurapintor <maura.pintor@gmail.com>
2024-03-04 15:13:17 +0100

    fixed tb example

--- examples/mnist_example_tensorboard.py
+++ examples/mnist_example_tensorboard.py
@@ -1,23 +1,24 @@
 import os
-from secml2.trackers.image_trackers import GradientsTracker, SampleTracker
-from secml2.trackers.trackers import (
+from secmlt.trackers import (
     GradientNormTracker,
     LossTracker,
     PredictionTracker,
     PerturbationNormTracker,
     TensorboardTracker,
+    GradientsTracker,
+    SampleTracker,
     ScoresTracker,
 )
 import torch
 import torchvision.datasets
 from torch.utils.data import DataLoader, Subset
 from robustbench.utils import download_gdrive
-from secml2.adv.backends import Backends
-from secml2.adv.evasion.pgd import PGD
-from secml2.adv.evasion.perturbation_models import PerturbationModels
+from secmlt.adv.backends import Backends
+from secmlt.adv.evasion.pgd import PGD
+from secmlt.adv.evasion.perturbation_models import PerturbationModels
 
-from secml2.metrics.classification import Accuracy
-from secml2.models.pytorch.base_pytorch_nn import BasePytorchClassifier
+from secmlt.metrics.classification import Accuracy
+from secmlt.models.pytorch.base_pytorch_nn import BasePytorchClassifier
 
 
 class MNISTNet(torch.nn.Module):
