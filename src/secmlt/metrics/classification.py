72e99c01a82ae00020d0903ad4ded86b0f3e917c
maurapintor <maura.pintor@gmail.com>
2024-03-07 12:55:57 +0100

    added ensemble sample-wise accuracy

--- src/secmlt/metrics/classification.py
+++ src/secmlt/metrics/classification.py
@@ -1,3 +1,4 @@
+from typing import List
 import torch
 from torch.utils.data import DataLoader
 
@@ -25,6 +26,36 @@ class Accuracy(object):
         self._num_samples += y_true.shape[0]
         self._accumulated_accuracy += torch.sum(
             y_pred.type(y_true.dtype).cpu() == y_true.cpu()
+        )
+
+    def compute(self):
+        return self._accumulated_accuracy / self._num_samples
+
+
+class SampleWiseAccuracy(object):
+    def __init__(self):
+        self._num_samples = 0
+        self._accumulated_accuracy = 0.0
+
+    def __call__(self, model: BaseModel, dataloaders: List[DataLoader]):
+        for advs in zip(*dataloaders):
+            y_pred = []
+            for x, y in advs:
+                y_pred.append(model.predict(x).cpu().detach())
+                # verify that the samples order correspond
+                assert (y - advs[0][1]).sum() == 0
+            y_pred = torch.vstack(y_pred)
+            self.accumulate(y_pred, advs[0][1])
+        accuracy = self.compute()
+        return accuracy
+
+    def accumulate(self, y_pred: torch.Tensor, y_true: torch.Tensor):
+        self._num_samples += y_true.shape[0]
+        self._accumulated_accuracy += torch.sum(
+            # take worst over predictions
+            (y_pred.type(y_true.dtype).cpu() == y_true.cpu())
+            .min(dim=0)
+            .values
         )
 
     def compute(self):
