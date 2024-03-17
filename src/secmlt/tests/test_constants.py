99a2b63163f2008a0b8e730a51baa2a99c15ff51
maurapintor <maura.pintor@gmail.com>
2024-03-17 16:12:29 +0100

    test for accuracy and metrics

--- src/secmlt/tests/test_constants.py
+++ src/secmlt/tests/test_constants.py
@@ -1,8 +1,16 @@
 from secmlt.adv.backends import Backends
+from secmlt.adv.evasion.perturbation_models import LpPerturbationModels
 
 
-def test_backends():
+def test_backends() -> None:
     assert hasattr(Backends, "FOOLBOX")
     assert hasattr(Backends, "NATIVE")
     assert Backends.FOOLBOX == "foolbox"
     assert Backends.NATIVE == "native"
+
+
+def test_perturbation_models() -> None:
+    assert hasattr(LpPerturbationModels, "L0")
+    assert hasattr(LpPerturbationModels, "L1")
+    assert hasattr(LpPerturbationModels, "L2")
+    assert hasattr(LpPerturbationModels, "LINF")
