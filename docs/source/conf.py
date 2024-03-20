04c429ee44f6b7df01053224ebbd3ed9efb101d8
maurapintor <maura.pintor@gmail.com>
2024-03-20 13:16:57 +0100

    fix conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,7 +6,11 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
 import pathlib
+import sys
+
+sys.path.insert(0, os.path.abspath("../../src"))
 
 
 project = "SecML-Torch"
@@ -25,11 +29,32 @@ release = version
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.doctest",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.todo",
+    "sphinx.ext.coverage",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.ifconfig",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.githubpages",
     "sphinx.ext.napoleon",
     "myst_parser",
 ]
 
+# Napoleon settings
 napoleon_google_docstring = False
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = True
+napoleon_include_private_with_doc = True
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = True
+
 napoleon_use_param = False
 napoleon_use_ivar = True
 
