Including correct L1 normalization

src/secmlt/optimization/gradient_processing.py
@@ -6,6 +6,8 @@ import torch.linalg
 from secmlt.adv.evasion.perturbation_models import LpPerturbationModels
 from torch.nn.functional import normalize
 
+from secmlt.utils.tensor_utils import normalize_l1_norm
+
 
 class GradientProcessing(ABC):
     """Gradient processing base class."""
@@ -79,7 +81,9 @@ class LinearProjectionGradientProcessing(GradientProcessing):
         NotImplementedError
             Raises NotImplementedError if the norm is not in 2, inf.
         """
-        if self.p in [1, 2]:
+        if self.p == 1:
+            return normalize_l1_norm(grad.data)
+        if self.p == 2:
             return normalize(grad.data, p=self.p, dim=0)
         if self.p == float("inf"):
             return torch.sign(grad)