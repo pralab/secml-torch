0acc73151808838a8c72a5f9107b6c00d127c1d6
maurapintor <maura.pintor@gmail.com>
2024-10-04 11:30:16 +0200

    removed replicated code for pgd

--- src/secmlt/adv/evasion/pgd.py
+++ src/secmlt/adv/evasion/pgd.py
@@ -1,7 +1,6 @@
 """Implementations of the Projected Gradient Descent evasion attack."""
 
 import importlib.util
-from functools import partial
 
 from secmlt.adv.backends import Backends
 from secmlt.adv.evasion.base_evasion_attack import (
@@ -96,7 +95,7 @@ class PGD(BaseEvasionAttackCreator):
         return [Backends.FOOLBOX, Backends.ADVLIB, Backends.NATIVE]
 
     @staticmethod
-    def _get_foolbox_implementation() -> type["PGDFoolbox"]:
+    def _get_foolbox_implementation() -> type["PGDFoolbox"]:  # noqa: F821
         if importlib.util.find_spec("foolbox", None) is not None:
             from secmlt.adv.evasion.foolbox_attacks.foolbox_pgd import PGDFoolbox
 
@@ -105,7 +104,7 @@ class PGD(BaseEvasionAttackCreator):
         raise ImportError(msg)
 
     @staticmethod
-    def _get_advlib_implementation() -> type["PGDAdvLib"]:
+    def _get_advlib_implementation() -> type["PGDAdvLib"]:  # noqa: F821
         if importlib.util.find_spec("adv_lib", None) is not None:
             from secmlt.adv.evasion.advlib_attacks import PGDAdvLib
 
@@ -116,208 +115,6 @@ class PGD(BaseEvasionAttackCreator):
     @staticmethod
     def _get_native_implementation() -> type["PGDNative"]:
         return PGDNative
-
-
-if importlib.util.find_spec("foolbox", None) is not None:
-    from secmlt.adv.evasion import BaseFoolboxEvasionAttack
-
-    class PGDFoolbox(BaseFoolboxEvasionAttack):
-        """Foolbox implementation of the PGD attack."""
-
-        def __init__(
-            self,
-            perturbation_model: str,
-            epsilon: float,
-            num_steps: int,
-            step_size: float,
-            random_start: bool,
-            y_target: int | None = None,
-            lb: float = 0.0,
-            ub: float = 1.0,
-            trackers: list[Tracker] | None = None,
-            **kwargs,
-        ) -> None:
-            """
-            Create Foolbox PGD attack.
-
-            Parameters
-            ----------
-            perturbation_model : str
-                Perturbation model for the attack. Available: 1, 2, inf.
-            epsilon : float
-                Radius of the constraint for the Lp ball.
-            num_steps : int
-                Number of iterations for the attack.
-            step_size : float
-                Attack step size.
-            random_start : bool
-                Whether to use a random initialization onto the Lp ball.
-            y_target : int | None, optional
-                Target label for a targeted attack, None
-                for untargeted attack, by default None.
-            lb : float, optional
-                Lower bound of the input space, by default 0.0.
-            ub : float, optional
-                Upper bound of the input space, by default 1.0.
-            trackers : list[Tracker] | None, optional
-                Trackers to check various attack metrics (see secmlt.trackers),
-                available only for native implementation, by default None.
-
-            Raises
-            ------
-            NotImplementedError
-                Raises NotImplementedError if the requested perturbation
-                model is not defined for this attack.
-            """
-            from foolbox.attacks import (
-                L1ProjectedGradientDescentAttack,
-                L2ProjectedGradientDescentAttack,
-                LinfProjectedGradientDescentAttack,
-            )
-
-            perturbation_models = {
-                LpPerturbationModels.L1: L1ProjectedGradientDescentAttack,
-                LpPerturbationModels.L2: L2ProjectedGradientDescentAttack,
-                LpPerturbationModels.LINF: LinfProjectedGradientDescentAttack,
-            }
-            foolbox_attack_cls = perturbation_models.get(perturbation_model)
-            if foolbox_attack_cls is None:
-                msg = "This perturbation model is not implemented in foolbox."
-                raise NotImplementedError(msg)
-
-            foolbox_attack = foolbox_attack_cls(
-                abs_stepsize=step_size,
-                steps=num_steps,
-                random_start=random_start,
-            )
-
-            super().__init__(
-                foolbox_attack=foolbox_attack,
-                epsilon=epsilon,
-                y_target=y_target,
-                lb=lb,
-                ub=ub,
-                trackers=trackers,
-            )
-
-        @staticmethod
-        def get_perturbation_models() -> set[str]:
-            """
-            Check the perturbation models implemented for the given attack.
-
-            Returns
-            -------
-            set[str]
-                The set of perturbation models for which the attack is implemented.
-
-            Raises
-            ------
-            NotImplementedError
-                Raises NotImplementedError if not implemented in the inherited class.
-            """
-            return {
-                LpPerturbationModels.L1,
-                LpPerturbationModels.L2,
-                LpPerturbationModels.LINF,
-            }
-
-
-if importlib.util.find_spec("adv_lib", None) is not None:
-    from secmlt.adv.evasion import BaseAdvLibEvasionAttack
-
-    class PGDAdvLib(BaseAdvLibEvasionAttack):
-        """Wrapper of the Adversarial Library implementation of the PGD attack."""
-
-        def __init__(
-            self,
-            perturbation_model: str,
-            epsilon: float,
-            num_steps: int,
-            random_start: bool,
-            step_size: float,
-            restarts: int = 1,
-            loss_function: str = "ce",
-            y_target: int | None = None,
-            lb: float = 0.0,
-            ub: float = 1.0,
-            **kwargs,
-        ) -> None:
-            """
-            Initialize a PGD attack with the Adversarial Library backend.
-
-            Parameters
-            ----------
-            perturbation_model : str
-                The perturbation model to be used for the attack.
-            epsilon : float
-                The maximum perturbation allowed.
-            num_steps : int
-                The number of iterations for the attack.
-            random_start : bool
-                If True, the perturbation will be randomly initialized.
-            step_size : float
-                The attack step size.
-            restarts : int, optional
-                The number of attack restarts. The default value is 1.
-            loss_function : str, optional
-                The loss function to be used for the attack. The default value is "ce".
-            y_target : int | None, optional
-                The target label for the attack. If None, the attack is
-                untargeted. The default value is None.
-            lb : float, optional
-                The lower bound for the perturbation. The default value is 0.0.
-            ub : float, optional
-
-            Raises
-            ------
-            ValueError
-                If the provided `loss_function` is not supported by the PGD attack
-                using the Adversarial Library backend.
-            """
-            from adv_lib.attacks import pgd_linf
-
-            perturbation_models = {
-                LpPerturbationModels.LINF: pgd_linf,
-            }
-            losses = ["ce", "dl", "dlr"]
-            if isinstance(loss_function, str):
-                if loss_function not in losses:
-                    msg = f"PGD AdvLib supports only these losses: {losses}"
-                    raise ValueError(msg)
-            else:
-                loss_function = losses[0]
-
-            advlib_attack_func = perturbation_models.get(perturbation_model)
-            advlib_attack = partial(
-                advlib_attack_func,
-                steps=num_steps,
-                random_init=random_start,
-                restarts=restarts,
-                loss_function=loss_function,
-                absolute_step_size=step_size,
-            )
-
-            super().__init__(
-                advlib_attack=advlib_attack,
-                epsilon=epsilon,
-                y_target=y_target,
-                lb=lb,
-                ub=ub,
-            )
-
-        @staticmethod
-        def get_perturbation_models() -> set[str]:
-            """
-            Check the perturbation models implemented for this attack.
-
-            Returns
-            -------
-            set[str]
-                The list of perturbation models implemented for this attack.
-            """
-            return {
-                LpPerturbationModels.LINF,
-            }
 
 
 class PGDNative(ModularEvasionAttackFixedEps):
