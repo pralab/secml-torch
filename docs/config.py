a23386d1bb2a58419be07304d210dbf73f0a1c58
maurapintor <maura.pintor@gmail.com>
2025-07-14 17:02:59 +0800

    fix issues with imports

--- docs/config.py
+++ docs/config.py
@@ -3,5 +3,5 @@ import sys
 
 # Add the library path: assumes 'docs/' is in the root, and 'src/' is sibling
 sys.path.insert(
-    0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "src"))
+    0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../..", "src"))
 )
