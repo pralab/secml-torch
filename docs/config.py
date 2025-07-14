d9ee66cb1ebdbff8525150223041bb6b0912c852
maurapintor <maura.pintor@gmail.com>
2025-07-14 16:59:17 +0800

    added sphinx conf

---
+++ docs/config.py
@@ -0,0 +1,7 @@
+import os
+import sys
+
+# Add the library path: assumes 'docs/' is in the root, and 'src/' is sibling
+sys.path.insert(
+    0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "src"))
+)
