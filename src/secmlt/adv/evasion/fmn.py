ba7f2030b64bbdf9b912a5ddc62724da889226df
maurapintor <maura.pintor@gmail.com>
2025-07-22 19:07:36 +0200

    added adv lib and foolbox implementations of FMN plus FMN creator class

---
+++ src/secmlt/adv/evasion/fmn.py
@@ -0,0 +1,196 @@
+"""Implementations of the Fast Minimum-Norm evasion attack."""
+
+from __future__ import annotations  # noqa: I001
+
+import importlib.util
+
+from secmlt.adv.backends import Backends
+from secmlt.adv.evasion.base_evasion_attack import (
+    BaseEvasionAttack,
+    BaseEvasionAttackCreator,
+)
+from secmlt.adv.evasion.modular_attack import LOGIT_LOSS, ModularEvasionAttackFixedEps
+from secmlt.adv.evasion.perturbation_models import LpPerturbationModels
+from secmlt.manipulations.manipulation import AdditiveManipulation
+from secmlt.optimization.constraints import (
+    ClipConstraint,
+    L0Constraint,
+    L1Constraint,
+    L2Constraint,
+    LInfConstraint,
+)
+from secmlt.optimization.gradient_processing import LinearProjectionGradientProcessing
+from secmlt.optimization.initializer import Initializer
+from secmlt.optimization.optimizer_factory import OptimizerFactory
+from secmlt.optimization.scheduler_factory import LRSchedulerFactory
+from typing import TYPE_CHECKING, Optional
+
+if TYPE_CHECKING:
+    from secmlt.trackers.trackers import Tracker
+
+
+class FMN(BaseEvasionAttackCreator):
+    """Creator for the Fast Minimum-Norm (FMN) attack."""
+
+    def __new__(
+        cls,
+        perturbation_model: str,
+        num_steps: int,
+        step_size: float,
+        min_step_size: Optional[float] = None,
+        gamma: float = 0.05,
+        y_target: int | None = None,
+        lb: float = 0.0,
+        ub: float = 1.0,
+        backend: str = Backends.FOOLBOX,
+        trackers: list[Tracker] | None = None,
+        **kwargs,
+    ) -> BaseEvasionAttack:
+        """
+        Create the FMN attack.
+
+        Parameters
+        ----------
+        perturbation_model : str
+            The perturbation model to be used for the attack.
+        num_steps : int
+            The number of iterations for the attack.
+        max_step_size : float
+            The attack step size.
+        min_step_size : float, optional
+            The number of attack restarts. The default value is 1.
+        gamma: float, optional
+            Step size for modifying the eps-ball. Will decay with cosine annealing.
+        y_target : int | None, optional
+            The target label for the attack. If None, the attack is
+            untargeted. The default value is None.
+        lb : float, optional
+            The lower bound for the perturbation. The default value is 0.0.
+        ub : float, optional
+            The upper bound for the perturbation. The default value is 1.0.
+        backend : str, optional
+            Backend to use to run the attack, by default Backends.FOOLBOX
+        trackers : list[Tracker] | None, optional
+            Trackers to check various attack metrics (see secmlt.trackers),
+            available only for native implementation, by default None.
+
+        Returns
+        -------
+        BaseEvasionAttack
+            FMN attack instance.
+        """
+        cls.check_backend_available(backend)
+        implementation = cls.get_implementation(backend)
+        implementation.check_perturbation_model_available(perturbation_model)
+        return implementation(
+            perturbation_model=perturbation_model,
+            num_steps=num_steps,
+            max_step_size=step_size,
+            min_step_size=min_step_size,
+            gamma=gamma,
+            y_target=y_target,
+            lb=lb,
+            ub=ub,
+            trackers=trackers,
+            **kwargs,
+        )
+
+    @staticmethod
+    def get_backends() -> list[str]:
+        """Get available implementations for the FMN attack."""
+        return [Backends.FOOLBOX, Backends.ADVLIB, Backends.NATIVE]
+
+    @staticmethod
+    def _get_foolbox_implementation() -> type[FMNFoolbox]:  # noqa: F821
+        if importlib.util.find_spec("foolbox", None) is not None:
+            from secmlt.adv.evasion.foolbox_attacks.foolbox_fmn import FMNFoolbox
+
+            return FMNFoolbox
+        msg = "Foolbox extra not installed"
+        raise ImportError(msg)
+
+    @staticmethod
+    def _get_advlib_implementation() -> type[FMNAdvLib]:  # noqa: F821
+        if importlib.util.find_spec("adv_lib", None) is not None:
+            from secmlt.adv.evasion.advlib_attacks import FMNAdvLib
+
+            return FMNAdvLib
+        msg = "Adversarial Library extra not installed"
+        raise ImportError(msg)
+
+    @staticmethod
+    def _get_native_implementation() -> type[FMNNative]:
+        return FMNNative
+
+
+class FMNNative(ModularEvasionAttackFixedEps):
+    """Native implementation of the Fast Minimum-Norm attack."""
+
+    def __init__(
+        self,
+        perturbation_model: str,
+        num_steps: int,
+        step_size: float,
+        y_target: int | None = None,
+        lb: float = 0.0,
+        ub: float = 1.0,
+        trackers: list[Tracker] | None = None,
+        **kwargs,
+    ) -> None:
+        """
+        Create Native FMN attack.
+
+        Parameters
+        ----------
+        perturbation_model : str
+            The perturbation model to be used for the attack.
+        num_steps : int
+            The number of iterations for the attack.
+        max_step_size : float
+            The attack step size.
+        min_step_size : float, optional
+            The number of attack restarts. The default value is 1.
+        gamma: float, optional
+            Step size for modifying the eps-ball. Will decay with cosine annealing.
+        y_target : int | None, optional
+            The target label for the attack. If None, the attack is
+            untargeted. The default value is None.
+        lb : float, optional
+            The lower bound for the perturbation. The default value is 0.0.
+        ub : float, optional
+            The upper bound for the perturbation. The default value is 1.0.
+        trackers : list[Tracker] | None, optional
+            Trackers to check various attack metrics (see secmlt.trackers),
+            available only for native implementation, by default None.
+        """
+        perturbation_models = {
+            LpPerturbationModels.L0: L0Constraint,
+            LpPerturbationModels.L1: L1Constraint,
+            LpPerturbationModels.L2: L2Constraint,
+            LpPerturbationModels.LINF: LInfConstraint,
+        }
+
+        initializer = Initializer()
+        gradient_processing = LinearProjectionGradientProcessing(
+            LpPerturbationModels.L2
+        )
+        perturbation_constraints = [
+            perturbation_models[perturbation_model](radius=self.epsilon),
+        ]
+        domain_constraints = [ClipConstraint(lb=lb, ub=ub)]
+        manipulation_function = AdditiveManipulation(
+            domain_constraints=domain_constraints,
+            perturbation_constraints=perturbation_constraints,
+        )
+        super().__init__(
+            y_target=y_target,
+            num_steps=num_steps,
+            step_size=step_size,
+            loss_function=LOGIT_LOSS,
+            optimizer_cls=OptimizerFactory.create_sgd(step_size),
+            lr_scheduler=LRSchedulerFactory.create_calr(),
+            manipulation_function=manipulation_function,
+            gradient_processing=gradient_processing,
+            initializer=initializer,
+            trackers=trackers,
+        )
---
+++ src/secmlt/adv/evasion/foolbox_attacks/foolbox_fmn.py
@@ -0,0 +1,94 @@
+"""Wrapper of the FMN attack implemented in Foolbox."""
+
+from __future__ import annotations
+
+from typing import Optional
+
+from foolbox.attacks.fast_minimum_norm import (
+    L0FMNAttack,
+    L1FMNAttack,
+    L2FMNAttack,
+    LInfFMNAttack,
+)
+from secmlt.adv.evasion.foolbox_attacks.foolbox_base import BaseFoolboxEvasionAttack
+from secmlt.adv.evasion.perturbation_models import LpPerturbationModels
+
+
+class FMNFoolbox(BaseFoolboxEvasionAttack):
+    """Wrapper of the Foolbox implementation of the FMN attack."""
+
+    def __init__(
+        self,
+        perturbation_model: str,
+        num_steps: int,
+        max_step_size: float,
+        min_step_size: Optional[float] = None,
+        gamma: float = 0.05,
+        y_target: int | None = None,
+        lb: float = 0.0,
+        ub: float = 1.0,
+        **kwargs,
+    ) -> None:
+        """
+        Create FMN attack with Foolbox backend.
+
+        Parameters
+        ----------
+        perturbation_model : str
+            The perturbation model to be used for the attack.
+        num_steps : int
+            The number of iterations for the attack.
+        max_step_size : float
+            The attack step size.
+        min_step_size : float, optional
+            The number of attack restarts. The default value is 1.
+        gamma: float, optional
+            Step size for modifying the eps-ball. Will decay with cosine annealing.
+        y_target : int | None, optional
+            The target label for the attack. If None, the attack is
+            untargeted. The default value is None.
+        lb : float, optional
+            The lower bound for the perturbation. The default value is 0.0.
+        ub : float, optional
+            The upper bound for the perturbation. The default value is 1.0.
+        """
+        perturbation_models = {
+            LpPerturbationModels.L0: L0FMNAttack,
+            LpPerturbationModels.L1: L1FMNAttack,
+            LpPerturbationModels.L2: L2FMNAttack,
+            LpPerturbationModels.LINF: LInfFMNAttack,
+        }
+        foolbox_attack_cls = perturbation_models.get(perturbation_model)
+
+        foolbox_attack = foolbox_attack_cls(
+            max_stepsize=max_step_size,
+            min_stepsize=min_step_size,
+            gamma=gamma,
+            steps=num_steps,
+        )
+
+        super().__init__(
+            foolbox_attack=foolbox_attack,
+            epsilon=None,
+            y_target=y_target,
+            lb=lb,
+            ub=ub,
+            **kwargs,
+        )
+
+    @staticmethod
+    def get_perturbation_models() -> set[str]:
+        """
+        Check the perturbation models implemented for this attack.
+
+        Returns
+        -------
+        set[str]
+            The list of perturbation models implemented for this attack.
+        """
+        return {
+            LpPerturbationModels.L0,
+            LpPerturbationModels.L1,
+            LpPerturbationModels.L2,
+            LpPerturbationModels.LINF,
+        }
