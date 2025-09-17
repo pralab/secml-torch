6a87d0b8bef34bb5f21cb1bffd5344dd987b4676
maurapintor <maura.pintor@gmail.com>
2025-09-17 19:42:11 +0200

    fix minor issues in tutorial rendering

--- docs/tutorials/utils/models.py
+++ docs/tutorials/utils/models.py
@@ -1,5 +1,6 @@
-# model definition for kWTA defense
+# model definition for kWTA defense and defensive distillation tutorials
 
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -132,3 +133,43 @@ class SparseResNet(nn.Module):
 
 def SparseResNet18(sparsities=[0.5, 0.4, 0.3, 0.2]):
     return SparseResNet(SparseBasicBlock, [2, 2, 2, 2], sparsities)
+
+
+class MNISTModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=(3, 3))
+        self.conv2 = nn.Conv2d(32, 32, kernel_size=(3, 3))
+        self.pool1 = nn.MaxPool2d(kernel_size=(2, 2))
+        self.conv3 = nn.Conv2d(32, 64, kernel_size=(3, 3))
+        self.conv4 = nn.Conv2d(64, 64, kernel_size=(3, 3))
+        self.pool2 = nn.MaxPool2d(kernel_size=(2, 2))
+        self.flatten = nn.Flatten()
+        self.linear1 = nn.Linear(1024, 200)
+        self.dropout = nn.Dropout(0.5)
+        self.linear2 = nn.Linear(200, 200)
+        self.linear3 = nn.Linear(200, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = torch.relu(x)
+        x = self.conv2(x)
+        x = torch.relu(x)
+        x = self.pool1(x)
+
+        x = self.conv3(x)
+        x = torch.relu(x)
+        x = self.conv4(x)
+        x = torch.relu(x)
+        x = self.pool2(x)
+
+        x = self.flatten(x)
+
+        x = self.linear1(x)
+        x = torch.relu(x)
+        x = self.dropout(x)
+
+        x = self.linear2(x)
+        x = torch.relu(x)
+
+        return self.linear3(x)
