2f10b5338803a62cb7a5e8c2584266ab63e3b87c
maurapintor <maura.pintor@gmail.com>
2026-03-12 13:13:03 +0100

    removed tests for unused modules fixing imports

--- src/secmlt/adv/evasion/__init__.py
+++ src/secmlt/adv/evasion/__init__.py
@@ -7,23 +7,23 @@ import types
 
 def _ensure_visdom_stub() -> None:
     """Provide a minimal visdom stub if visdom is unavailable."""
-    if importlib.util.find_spec("visdom", None) is not None:
+    if importlib.util.find_spec("visdom", None) is not None:  # pragma: no cover
         return
-    if "visdom" in sys.modules:
+    if "visdom" in sys.modules:  # pragma: no cover
         return
 
     visdom_stub = types.ModuleType("visdom")
 
     class Visdom:
         def __init__(self, *args, **kwargs) -> None:
-            return
+            pass
 
     visdom_stub.Visdom = Visdom
     sys.modules["visdom"] = visdom_stub
 
-if importlib.util.find_spec("foolbox", None) is not None:
+if importlib.util.find_spec("foolbox", None) is not None:  # pragma: no cover
     from .foolbox_attacks import *  # noqa: F403
 
-if importlib.util.find_spec("adv_lib", None) is not None:
+if importlib.util.find_spec("adv_lib", None) is not None:  # pragma: no cover
     _ensure_visdom_stub()
     from .advlib_attacks import *  # noqa: F403
