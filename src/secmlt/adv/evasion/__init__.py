d1f2d98379b25d2759e6da622893464ed711d40e
maurapintor <maura.pintor@gmail.com>
2026-03-12 10:51:07 +0100

    ghost visdom in init function

--- src/secmlt/adv/evasion/__init__.py
+++ src/secmlt/adv/evasion/__init__.py
@@ -1,9 +1,29 @@
 """Evasion attack functionalities."""
 
 import importlib.util
+import sys
+import types
+
+
+def _ensure_visdom_stub() -> None:
+    """Provide a minimal visdom stub if visdom is unavailable."""
+    if importlib.util.find_spec("visdom", None) is not None:
+        return
+    if "visdom" in sys.modules:
+        return
+
+    visdom_stub = types.ModuleType("visdom")
+
+    class Visdom:  # noqa: D101
+        def __init__(self, *args, **kwargs) -> None:  # noqa: ANN002, ANN003, ARG002
+            return
+
+    visdom_stub.Visdom = Visdom
+    sys.modules["visdom"] = visdom_stub
 
 if importlib.util.find_spec("foolbox", None) is not None:
     from .foolbox_attacks import *  # noqa: F403
 
 if importlib.util.find_spec("adv_lib", None) is not None:
+    _ensure_visdom_stub()
     from .advlib_attacks import *  # noqa: F403
