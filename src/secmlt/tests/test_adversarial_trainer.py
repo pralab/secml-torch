3544023ae86886bdefb7a5860050844b599725e7
Fabio Brau (MAC) <fabio.brau@unica.it>
2026-05-18 17:20:43 +0200

    Add test of mix branch in if #184

--- src/secmlt/tests/test_adversarial_trainer.py
+++ src/secmlt/tests/test_adversarial_trainer.py
@@ -1,3 +1,4 @@
+import pytest
 import torch
 from secmlt.adv.evasion.perturbation_models import LpPerturbationModels
 from secmlt.adv.evasion.pgd import PGDNative
@@ -58,3 +59,23 @@ def test_adversarial_trainer_with_classifier() -> None:
     trainer = AdversarialTrainer(optimizer=optimizer, loss=criterion, epochs=1)
     trained_model = trainer.train(classifier, data_loader, attack)
     assert isinstance(trained_model, torch.nn.Module)
+
+
+def test_adversarial_trainer_mix_mode_not_implemented() -> None:
+    # 1. Create random dataset
+    data = torch.randn(20, 3, 32, 32).clamp(0, 1)
+    labels = torch.randint(0, 9, (20,))
+    data_loader = DataLoader(TensorDataset(data, labels), batch_size=10)
+
+    # 2. Create a fake model with fake gradient
+    pytorch_model = MockModel()
+
+    # 3. Instantiate the PGD attack
+    attack = _make_attack()
+
+    # 4. Run the trainer with the "mix" combining mode and expect it to fail
+    optimizer = SGD(pytorch_model.parameters(), lr=0.01)
+    criterion = MockLoss()
+    trainer = AdversarialTrainer(optimizer=optimizer, loss=criterion, epochs=1)
+    with pytest.raises(NotImplementedError):
+        trainer.train(pytorch_model, data_loader, attack, combining_mode="mix")
